0d3ff0f14084742fcd0c77c4f93eb4d02585af89
Sawyer Diamond <sawyerrdiamond@gmail.com>
2024-05-25 10:48:31 -0400

    Implemented some changes to the grid.

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -25,20 +25,18 @@ const Work = () => {
           <h1>Projects</h1>
         </div>
         <div className="project__grid">
-          <div className="project__profile">
-            {projects.map((project, index) => (
-              <motion.div
-                whileInView={{ opacity: 1 }}
-                key={project.title + index}
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.9 }}
-                transition={{ duration: 0.5, type: "tween" }}
-                className="project__profile-item">
-                <img src={urlFor(project.imgUrl)} alt={project.title} />
-                <h3 className="project__profile-title">{project.title}</h3>
-              </motion.div>
-            ))}
-          </div>
+          {projects.map((project, index) => (
+            <motion.div
+              whileInView={{ opacity: 1 }}
+              key={project.title + index}
+              whileHover={{ scale: 1.01 }}
+              whileTap={{ scale: 0.9 }}
+              transition={{ duration: 0.5, type: "tween" }}
+              className="project__profile-item">
+              <img src={urlFor(project.imgUrl)} alt={project.title} />
+              <h3 className="hidden project__profile-title">{project.title}</h3>
+            </motion.div>
+          ))}
         </div>
       </div>
     </section>
