92462b4bcbd5755a540486656ffdf883a179145f
Sawyer Diamond <sawyerrdiamond@gmail.com>
2024-06-10 17:15:04 -0400

    BG Change

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -1,5 +1,5 @@
 import logo from "../assets/Logo.webp";
-import hero_bg from "../assets/HeroBG.webp";
+import hero_bg from "../assets/HeroBGD.webp";
 import hero_bg_t from "../assets/HeroBG-T.webp";
 import hero_bg_m from "../assets/HeroBG-M.webp";
 
