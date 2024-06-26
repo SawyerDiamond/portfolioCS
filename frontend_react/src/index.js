289e55f969ea85f5f131db0e963ed311a988377d
Sawyer Diamond <sawyerrdiamond@gmail.com>
2024-06-26 10:25:06 -0400

    Prior to switch...

--- frontend_react/src/index.js
+++ frontend_react/src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
-
-import App from "./App";
-import "./index.css";
-
 import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App tab="home" />);
+root.render(<App />);
