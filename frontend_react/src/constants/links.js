af33f72739e190a1fc59d2178716b90d4ce5715f
Sawyer Diamond <sawyerrdiamond@gmail.com>
2024-07-05 21:12:09 -0400

    Updated broken link.

--- frontend_react/src/constants/links.js
+++ frontend_react/src/constants/links.js
@@ -2,7 +2,7 @@ const links = {
   LinkedIn: "https://linkedin.com/in/sawyer-diamond-76932b294",
   GitHub: "https://github.com/SawyerDiamond",
   Resume:
-    "https://drive.google.com/file/d/1P5Uvh28PN2Z35h9xZIu5Yad683JJ56IG/view?usp=sharing",
+    "https://drive.google.com/file/d/1OwnFkRBshSSMrV3hbgXgNcCtOM8u1du-/view?usp=sharing",
   Mail: "mailto:sawyerrdiamond@gmail.com",
 };
 
