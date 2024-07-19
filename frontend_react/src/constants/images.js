39b0b3a61211a2f70be1f5b0f3c69615cebcd593
Sawyer Diamond <sawyerrdiamond@gmail.com>
2024-07-19 01:32:36 -0400

    Added petals.

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -3,12 +3,18 @@ import hero_bg from "../assets/HeroBGD.webp";
 import hero_bg_t from "../assets/HeroBG-T.webp";
 import hero_bg_m from "../assets/HeroBG-M.webp";
 import logo2 from "../assets/Logo2.svg";
+import bluepetal from "../assets/Blue_Petal.svg";
+import goldpetal from "../assets/Gold_Petal.svg";
+import pinkpetal from "../assets/Pink_Petal.svg";
 const images = {
   logo,
   hero_bg,
   hero_bg_t,
   hero_bg_m,
   logo2,
+  bluepetal,
+  goldpetal,
+  pinkpetal,
 };
 
 export default images;
--- frontend_react/src/constants/links.js
+++ frontend_react/src/constants/links.js
@@ -2,7 +2,7 @@ const links = {
   LinkedIn: "https://linkedin.com/in/sawyer-diamond-76932b294",
   GitHub: "https://github.com/SawyerDiamond",
   Resume:
-    "https://drive.google.com/file/d/1OwnFkRBshSSMrV3hbgXgNcCtOM8u1du-/view?usp=sharing",
+    "https://drive.google.com/drive/folders/1NndzQSv-8ihHEga-tO9Se0ibH0eAM0jo?usp=share_link",
   Mail: "mailto:sawyerrdiamond@gmail.com",
 };
 
