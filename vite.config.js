3d90e6bde4b579d89ea5ca5f790474ed7e7e627e
Sawyer Diamond <sawyerrdiamond@gmail.com>
2026-02-12 12:09:38 -0500

    cleanup: Remove Sanity backend dependencies and configuration

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 export default defineConfig({
   base: "/portfolioCS/",
   plugins: [react()],
   esbuild: {
     loader: "jsx",
-  },
-  define: {
-    "import.meta.env.VITE_SANITY_PROJECT_ID": JSON.stringify("295d7tee"),
-    "import.meta.env.VITE_SANITY_TOKEN": JSON.stringify(
-      process.env.VITE_SANITY_TOKEN,
-    ),
   },
 });
