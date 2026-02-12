50cc87b5bb20d770d7acf202ce3462401d0efb5c
Sawyer Diamond <sawyerrdiamond@gmail.com>
2026-02-12 12:13:32 -0500

    fix: Change base path to / for custom domain support

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/portfolioCS/",
+  base: "/",
   plugins: [react()],
   esbuild: {
     loader: "jsx",
